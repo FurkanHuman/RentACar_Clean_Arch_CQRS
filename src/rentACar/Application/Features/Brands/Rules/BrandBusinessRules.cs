dbf6de05f5c6552480b65c0562a145dcb60af56d
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-29 21:46:17 +0300

    day 2 full

---
+++ src/rentACar/Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -0,0 +1,23 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+
+namespace Application.Features.Brands.Rules
+{
+    public class BrandBusinessRules
+    {
+        private readonly IBrandRepository _brandRepository;
+
+        public BrandBusinessRules(IBrandRepository brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+
+        public async Task BrandNameCanNotBeDublicatedWhenInserted(string name)
+        {
+            IPaginate<Brand> resut=await _brandRepository.GetListAsync(b=>b.Name==name);
+            if (resut.Items.Any()) throw new BusinessException("Brand Name Exit.");
+        }
+    }
+}
